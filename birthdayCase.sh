e492f1a53bb154f7e26da2b9db4eed84ad442822
Durgesh094 <durgeshchavan1994@gmail.com>
2022-05-09 11:25:39 +0530

    Added: Practice problems on dictionary

---
+++ birthdayCase.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+month=("Jan" "Feb" "Mar" "Apr" "May" "Jun" "july" "Aug" "Sept" "Oct" "Nov" "Dec");
+year=(92 93);
+
+random=$((RANDOM%24));
+
+echo $random;
+
+echo "Key :" ${month[random%12]}${year[random/12]};
---
+++ birthdayexample.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+declare -A birthday;
+
+month=("Jan" "Feb" "Mar" "Apr" "May" "Jun" "july" "Aug" "Sept" "Oct" "Nov" "Dec");
+year=(92 93);
+
+for ((i=0;i<${#month[@]};i++))
+do
+	for((j=0;j<${#year[@]};j++))
+	do
+		birthday[${month[i]}${year[j]}]=0;
+	done
+done
+
+#echo ${birthday[@]};
+echo ${!birthday[@]};
